12e36ce99e1721a1d118fcd6acd753ab25e46096
Ariana Adames <aiadames@mit.edu>
2020-06-13 15:37:24 -0400

    firebase

---
+++ script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCQvNFLiPhhX06gKfnEEXjPngYPTrC7o34",
+    authDomain: "prosfero-tech.firebaseapp.com",
+    databaseURL: "https://prosfero-tech.firebaseio.com",
+    projectId: "prosfero-tech",
+    storageBucket: "prosfero-tech.appspot.com",
+    messagingSenderId: "848332702195",
+    appId: "1:848332702195:web:776d4481a88f03f55c5286"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
